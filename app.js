daad91750454cdaa6aded0fe73770622a21a8623
parshev <s.parshe19@gmail.com>
2019-07-15 20:56:55 +0300

    update

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-var counter = 1;
-var taskArr = [];
-function setCounter(num){
-    return num +=1;
-}
-function initial(){
-    console.log('ok');
-    document.getElementById('numOfTask').innerHTML = counter;
+var counter = 0;
+function setColor(){
+    var body = document.getElementById('body');
+    body.style.backgroundColor = 'gray';
+    
 }
 
-function func1(){
+
+function createTask(){
     
-    var number ;
-    var task = document.getElementById('taskString').value;//task text
-    console.log('task: '+task);
-    
-    if(task){
-        number = setCounter(counter);//nomer na zadacha
-        counter = number;//uvelichava countr s 1
-        console.log('counter is: ' + counter)//display counter
-        var not = document.getElementById('numOfTask').innerHTML = counter;//set nomer na zadacha na counter
-        console.log('task number: ' + number);//display number
-        taskArr[number-1]=task;//set v masiva s index number stoinost task
-        //console.log('true');
-        //console.log(not);
-        console.log('length of taskArr is : ' + taskArr.length);
+    var bodyContent = document.getElementById('bodyContent');
+    var inputTask = document.getElementById('inputTask').value;
+    var newInputTask = document.createElement('p');
+    var doneBtn = document.createElement('input');
+    //doneBtn.setAttribute('type','checkbox');
+    console.log(newInputTask);
+    var paragraph = document.getElementsByTagName('p');
+  ;
+    if(inputTask){
+        var taskNumber = counter +1;
+        //taskLengthValidation(inputTask,newInputTask);
+        newInputTask.innerHTML = taskNumber+" . "+inputTask//funkciq za proverka na daljinata i ako e po golqma ot poleto reset razmerite na p;
+        
+        counter += 1;
+        newInputTask.setAttribute('id',counter);
+        newInputTask.setAttribute('class','inClass');
+        //newInputTask.style.backgroundColor = '#339966';
+        
+        
+        
+        bodyContent.appendChild(newInputTask);
+       // bodyContent.appendChild(doneBtn);
+        document.getElementById('inputTask').value='';
+        console.log('num of task: '+counter+'\ncontent: '+inputTask)
+    }
+    newInputTask.addEventListener("mouseover", function( event ) {   
+  event.target.style.backgroundColor = "#339966";
+    });
+    newInputTask.addEventListener("mouseout", function( event ) {   
+  event.target.style.backgroundColor = "gray";
+    });
+    newInputTask.addEventListener("onclick", function( event ) {   
+  console.log(sevent.target);
+    }); 
+}
+function taskLengthValidation(inputTask,newInputTask){
+    if(inputTask.length>10){
+            console.log('else state or taskLenthValid funk');
+            console.log(inputTask.length);
+            newInputTask.setAttribute('width',inputTask.length);
+        
     }else{
-        console.log('false');
-    }
-    var taskText = document.getElementById('taskString').value ='';
-    console.log(' out of if length of taskArr is : ' + taskArr.length);   
-}
-function func2(){
-    for(var i = 1; i<taskArr.length; i++){
-        console.log(taskArr[i]);
+        console.log('else state or taskLenthValid funk');
+        console.log(inputTask.length);
     }
 }
-
-
-
-// if(taskText===undefined){
-//        console.log('none tasktext');
-//        console.log('task name: ' + task + '\t');
-//        console.log('task number: ' + number);
-//        console.log('length of taskArr is : ' + taskArr.length);
-//        console.log(not);
-//    }
